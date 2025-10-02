4b25edc0f1340f06d63e8cf429b0b9ec50a57957
Jer-Main-e <straverspinda@gmail.com>
2025-10-03 00:32:43 +0200

    an enum Role has been added to the Client model file so that each user will be identified by a given role in the future.
    Some written out logic has been added to the BoughtProductsService file.
    
    The admin role has been given to client3

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -5,10 +5,20 @@ namespace Grocery.Core.Models
     {
         public string EmailAddress { get; set; }
         public string Password { get; set; }
-        public Client(int id, string name, string emailAddress, string password) : base(id, name)
+
+        public enum Role
+        {
+            None,
+            Admin
+        }
+
+        public Role UserRole { get; set; }
+
+        public Client(int id, string name, string emailAddress, string password, Role role = Role.None) : base(id, name)
         {
             EmailAddress=emailAddress;
             Password=password;
+            UserRole=role;
         }
     }
 }
--- Grocery.Core/Services/BoughtProductsService.cs
+++ Grocery.Core/Services/BoughtProductsService.cs
@@ -2,6 +2,7 @@
 using Grocery.Core.Interfaces.Repositories;
 using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Grocery.Core.Services
 {
@@ -20,7 +21,28 @@ namespace Grocery.Core.Services
         }
         public List<BoughtProducts> Get(int? productId)
         {
-            throw new NotImplementedException();
+            List<BoughtProducts> boughtProductsList = new List<BoughtProducts>();
+
+            //In BoughtProductsService werk je de Get(productid) functie uit,
+            //zodat alle Clients die product met productid hebben gekocht met client,
+            //boodschappenlijst en product in de lijst staan die wordt geretourneerd.
+
+            //Men gaat met het productID bekijken in welke boodschappen lijst het
+            //product zich bevindt. Vervolgens word er gekeken van welke client deze
+            //boodschappen lijst is. Wanneer de lijst van een user is die niet de rol
+            //Admin heeft, dan wordt de boodschappenlijst aan de boughtProductList toegevoegd
+            //samen met het Client object en het product.
+
+            //voordat men door de lijst met gekochte producten gaat kijken gaat men eerst
+            //bepalen welke lijsten van Admins zijn, want deze gegevens willen we niet opslaan.
+
+            
+
+            _groceryListItemsRepository.GetAll().Where(item => item.ProductId == productId);
+            
+
+
+            return boughtProductsList;
         }
     }
 }
