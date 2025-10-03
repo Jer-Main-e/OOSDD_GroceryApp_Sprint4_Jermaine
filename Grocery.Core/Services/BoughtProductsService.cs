f5708377b041ed980e8c73d42604d7247c4f6762
Jer-Main-e <straverspinda@gmail.com>
2025-10-03 23:45:27 +0200

    Feature/klanten tonen per product (#2)
    * A small start to the popular products list has been made the table headers and GetBestSellingProducts function were added, though they aren't functioning as desired at the moment.
    
    * Edited the bestSellingProducts page. Now we can actively see that the list is being correctly updated.
    
    * an enum Role has been added to the Client model file so that each user will be identified by a given role in the future.
    
    Some written out logic has been added to the BoughtProductsService file.
    
    The admin role has been given to client3
    
    * De navigatie naar de gekochte producten page is correct toegevoegd. Nu kan alleen de admin navigeren naar de gekochte producten page.
    
    * De namen van de producten worden nu correct weergeven voor de Admin.
    
    * Gehele functionaliteit is geïmplementeerd en de lijst met gekochte producten wordt geüpdate. De gekochte producten van de admin worden niet weergeven volgens wens.

--- Grocery.Core/Services/BoughtProductsService.cs
+++ Grocery.Core/Services/BoughtProductsService.cs
@@ -2,6 +2,7 @@
 using Grocery.Core.Interfaces.Repositories;
 using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Grocery.Core.Services
 {
@@ -20,7 +21,41 @@ namespace Grocery.Core.Services
         }
         public List<BoughtProducts> Get(int? productId)
         {
-            throw new NotImplementedException();
+            List<BoughtProducts> boughtProductsList = new List<BoughtProducts>();
+
+            //In BoughtProductsService werk je de Get(productid) functie uit,
+            //zodat alle Clients die product met productid hebben gekocht met client,
+            //boodschappenlijst en product in de lijst staan die wordt geretourneerd.
+
+            //Men gaat met het productID bekijken in welke boodschappen lijst het
+            //product zich bevindt. Vervolgens word er gekeken van welke client deze
+            //boodschappen lijst is. Wanneer de lijst van een user is die niet de rol
+            //Admin heeft, dan wordt de boodschappenlijst aan de boughtProductList toegevoegd
+            //samen met het Client object en het product.
+
+            //voordat men door de lijst met gekochte producten gaat kijken gaat men eerst
+            //bepalen welke lijsten van Admins zijn, want deze gegevens willen we niet opslaan.
+            var allViableClients = _clientRepository.GetAll().Where(client => client.UserRole == Client.Role.None).ToList();
+
+            
+
+            var allViableGroceryListsIds = _groceryListRepository.GetAll().Where(groceryList => allViableClients.Select(vC => vC.Id).Contains(groceryList.ClientId)).Select(groceryList => groceryList.Id).ToList();
+
+
+            foreach(int id in allViableGroceryListsIds)
+            {
+                var currentGroceryList = _groceryListRepository.Get(id);
+                var allViableProducts = _groceryListItemsRepository.GetAllOnGroceryListId(id);
+                foreach (var viableProduct in allViableProducts) 
+                {
+                    if (viableProduct.ProductId != productId) { 
+                        continue;
+                    }
+                    BoughtProducts boughtProduct = new BoughtProducts(_clientRepository.Get(currentGroceryList.ClientId), currentGroceryList, viableProduct.Product);
+                    boughtProductsList.Add(boughtProduct);
+                }
+            }
+            return boughtProductsList;
         }
     }
 }
